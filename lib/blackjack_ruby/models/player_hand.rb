caa71c8c62c87dab7f6ac67f36578a35df229940
Thad Le <llengocthangg@gmail.com>
2023-11-30 04:56:32 +1100

    Refactor PlayerHand
    - Add #must_hit?, #must_stay?, #hit_or_stay?
    - Rewrite #options base on newly added methods

--- lib/blackjack_ruby/models/player_hand.rb
+++ lib/blackjack_ruby/models/player_hand.rb
@@ -4,15 +4,39 @@ module BlackjackRuby
       # 0 => 'Stay'
       # 1 => 'Hit'
       def options
-        if blackjack? || enough? || busted?
+        if must_stay?
           return [0]
         end
 
-        [1]
+        if must_hit?
+          return [1]
+        end
+
+        if hit_or_stay?
+          return [0, 1]
+        end
+
+        raise "Unknown known hand: #{cards.map(&:to_encoded_unicode).join(' ')}"
       end
 
-      def enough?
-        scores.any? { |s| s >= 12 && s <= 21 }
+      # def enough?
+      #   scores.any? { |s| s >= 12 && s <= 21 }
+      # end
+
+      def hit_or_stay?
+        !blackjack? && scores.any? { |s| s < 21 }
+      end
+
+      def must_hit?
+        !blackjack? && scores.all? { |s| s < 12 }
+      end
+
+      def must_stay?
+        blackjack? || busted? || twenty_one?
+      end
+
+      def twenty_one?
+        !blackjack? && scores.any? { |s| s == 21 }
       end
 
       def only_one_option?
