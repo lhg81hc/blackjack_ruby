6b8af22535dd2d2b427426fb9a301166a92bcb48
Thad Le <llengocthangg@gmail.com>
2023-11-30 16:33:40 +1100

    Refactor DealerHand#options
    - Change from return value from an array to a hash

--- lib/blackjack_ruby/models/dealer_hand.rb
+++ lib/blackjack_ruby/models/dealer_hand.rb
@@ -2,31 +2,17 @@ module BlackjackRuby
   module Models
     class DealerHand < Hand
       def options
-        if must_stay?
-          return [0]
-        end
-
-        if must_hit?
-          return [1]
-        end
-
-        raise "Unknown known hand: #{cards.map(&:to_encoded_unicode).join(' ')}"
+        {
+          'stay' => can_stay?
+        }
       end
 
       def enough?
         scores.any? { |s| s >= 17 && s <= 21 }
       end
 
-      def must_hit?
-        !blackjack? && best_score < 17
-      end
-
-      def must_stay?
+      def can_stay?
         blackjack? || busted? || enough?
-      end
-
-      def only_one_option?
-        options.count == 1
       end
     end
   end
