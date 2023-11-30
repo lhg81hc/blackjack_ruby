f2b0263b71ec6474df4b73763051cd5c2b76d7e4
Thad Le <llengocthangg@gmail.com>
2023-11-30 16:34:05 +1100

    Refactor PlayerHand#options
    - Change return value from an array to a hash

--- lib/blackjack_ruby/models/player_hand.rb
+++ lib/blackjack_ruby/models/player_hand.rb
@@ -1,50 +1,22 @@
 module BlackjackRuby
   module Models
     class PlayerHand < Hand
-      # 0 => 'Stay'
-      # 1 => 'Hit'
+
       def options
-        if must_stay?
-          return [0]
-        end
+        {
+          'hit' => can_hit?,
+          'split' => can_split?,
+          'stay' => true,
+        }
 
-        if must_hit?
-          return [1]
-        end
-
-        if hit_or_stay?
-          return [0, 1]
-        end
-
-        raise "Unknown known hand: #{cards.map(&:to_encoded_unicode).join(' ')}"
       end
 
-      # def enough?
-      #   scores.any? { |s| s >= 12 && s <= 21 }
-      # end
-
-      def hit_or_stay?
-        !blackjack? && scores.any? { |s| s < 21 }
-      end
-
-      def must_hit?
-        !blackjack? && scores.all? { |s| s < 12 }
-      end
-
-      def must_stay?
-        blackjack? || busted? || twenty_one?
-      end
-
-      def twenty_one?
-        !blackjack? && scores.any? { |s| s == 21 }
-      end
-
-      def splitable?
+      def can_split?
         two_cards? && identical_score_cards?
       end
 
-      def only_one_option?
-        options.count == 1
+      def can_hit?
+        !blackjack? && scores.any? { |s| s < Hand::BUST_VALUE }
       end
     end
   end
