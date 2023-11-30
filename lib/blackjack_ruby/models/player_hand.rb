e0ff7bd7d9c084fe7a24c0c59adba9ded11836f1
Thad Le <llengocthangg@gmail.com>
2023-11-30 18:51:53 +1100

    Add more attributes to PlayerHand
    - @doubled flag to represent the hand is doubled or not
    - @stated flag to represent if the player decides to stay
    - @initial_payout_odds to handle Blackjack payout odds
    - @bet to represent the bet amount

--- lib/blackjack_ruby/models/player_hand.rb
+++ lib/blackjack_ruby/models/player_hand.rb
@@ -1,14 +1,26 @@
 module BlackjackRuby
   module Models
     class PlayerHand < Hand
+      attr_writer :doubled
+      attr_reader :initial_payout_odds
+      attr_accessor :bet, :stayed
+
+      def initialize(cards)
+        super
+
+        @doubled = false
+        @stayed = false
+        @initial_payout_odds = 1
+        @bet = 0
+      end
 
       def options
         {
+          'double' => can_double?,
           'hit' => can_hit?,
           'split' => can_split?,
           'stay' => true,
         }
-
       end
 
       def can_split?
@@ -16,7 +28,19 @@ module BlackjackRuby
       end
 
       def can_hit?
-        !blackjack? && scores.any? { |s| s < Hand::BUST_VALUE }
+        !blackjack? && !doubled? && scores.any? { |s| s < Hand::BUST_VALUE }
+      end
+
+      def can_double?
+        two_cards? && can_hit?
+      end
+
+      def doubled?
+        @doubled
+      end
+
+      def payout_odds
+        blackjack? ? (initial_payout_odds * 3 / 2) : initial_payout_odds
       end
     end
   end
