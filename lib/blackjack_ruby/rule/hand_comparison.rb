b8b03c63f1039ee5cb3a44b6527da39ce039c5f4
Thad Le <llengocthangg@gmail.com>
2023-12-05 04:45:52 +1100

    Introduce Rule::HandComparison

---
+++ lib/blackjack_ruby/rule/hand_comparison.rb
@@ -0,0 +1,17 @@
+module BlackjackRuby
+  module Rule
+    class HandComparison
+      def five_card_charlie
+        true
+      end
+
+      def player_blackjack_wins_automatically
+        true
+      end
+
+      def player_twentyone_wins_automatically
+        true
+      end
+    end
+  end
+end
