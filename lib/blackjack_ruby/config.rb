b2839e9bec0ed8ff472331cf2e207eed7823c0b4
Thad Le <llengocthangg@gmail.com>
2023-12-14 22:56:06 +1100

    Change #maximum_double_per_betting_box to

--- lib/blackjack_ruby/config.rb
+++ lib/blackjack_ruby/config.rb
@@ -16,14 +16,14 @@ module BlackjackRuby
 
     extend Option
 
-    option :dealer_hits_on_soft_seventeen,  default: BlackjackRuby::Rule::DealerHand::HIT_ON_SOFT_SEVENTEEN
-    option :five_card_charlie,              default: BlackjackRuby::Rule::HandComparison::FIVE_CARD_CHARLIE
-    option :player_bj_wins_automatically,   default: BlackjackRuby::Rule::HandComparison::PLAYER_BJ_WINS_AUTOMATICALLY
-    option :player_21_wins_automatically,   default: BlackjackRuby::Rule::HandComparison::PLAYER_21_WINS_AUTOMATICALLY
-    option :winner_when_same_best_score,    default: BlackjackRuby::Rule::HandComparison::WINNER_WHEN_SAME_BEST_SCORE
-    option :blackjack_payout_odds,          default: BlackjackRuby::Rule::PlayerHand::BLACKJACK_PAYOUT_ODDS
-    option :maximum_cards_allow_double,     default: BlackjackRuby::Rule::PlayerHand::MAXIMUM_CARDS_ALLOW_DOUBLE
-    option :maximum_double_per_betting_box, default: BlackjackRuby::Rule::PlayerHand::MAXIMUM_DOUBLE_PER_BETTING_BOX
-    option :number_of_decks,                default: BlackjackRuby::Rule::Shoe::NUMBER_OF_DECKS
+    option :dealer_hits_on_soft_seventeen,     default: BlackjackRuby::Rule::DealerHand::HIT_ON_SOFT_SEVENTEEN
+    option :five_card_charlie,                 default: BlackjackRuby::Rule::HandComparison::FIVE_CARD_CHARLIE
+    option :player_bj_wins_automatically,      default: BlackjackRuby::Rule::HandComparison::PLAYER_BJ_WINS_AUTOMATICALLY
+    option :player_21_wins_automatically,      default: BlackjackRuby::Rule::HandComparison::PLAYER_21_WINS_AUTOMATICALLY
+    option :winner_when_same_best_score,       default: BlackjackRuby::Rule::HandComparison::WINNER_WHEN_SAME_BEST_SCORE
+    option :blackjack_payout_odds,             default: BlackjackRuby::Rule::PlayerHand::BLACKJACK_PAYOUT_ODDS
+    option :maximum_cards_allow_double,        default: BlackjackRuby::Rule::PlayerHand::MAXIMUM_CARDS_ALLOW_DOUBLE
+    option :maximum_splitting_per_betting_box, default: BlackjackRuby::Rule::PlayerHand::MAXIMUM_SPLITTING_PER_BETTING_BOX
+    option :number_of_decks,                   default: BlackjackRuby::Rule::Shoe::NUMBER_OF_DECKS
   end
 end
--- lib/blackjack_ruby/rule/player_hand.rb
+++ lib/blackjack_ruby/rule/player_hand.rb
@@ -3,7 +3,7 @@ module BlackjackRuby
     class PlayerHand
       BLACKJACK_PAYOUT_ODDS = (3 / 2).to_f
       MAXIMUM_CARDS_ALLOW_DOUBLE = 3
-      MAXIMUM_DOUBLE_PER_BETTING_BOX = 2
+      MAXIMUM_SPLITTING_PER_BETTING_BOX = 2
     end
   end
 end
