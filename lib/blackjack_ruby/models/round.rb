34be37ec6f4fdeb8058470c0245a9a57b38d0670
Thad Le <llengocthangg@gmail.com>
2023-12-19 22:44:07 +1100

    Add :player_hands and :dealer_hand to round

--- lib/blackjack_ruby/models/round.rb
+++ lib/blackjack_ruby/models/round.rb
@@ -5,22 +5,30 @@ module BlackjackRuby
     class Round
       extend Forwardable
 
-      attr_reader :shoe_of_cards, :betting_boxes
+      attr_reader :shoe_of_cards, :betting_boxes, :player_hands, :dealer_hand
 
       def initialize(shoe_of_cards, betting_boxes)
         @shoe_of_cards = shoe_of_cards
         @betting_boxes = betting_boxes
+        @player_hands = []
+        @dealer_hand = nil
       end
 
-      def start
+      def setup
         validate_shoe_of_cards
         validate_betting_boxes
-        # deal card for each hand in each betting boxes
-        # evaluate the result on each hand of betting box
-        # pay out the winning hands
-        # deal card for dealer
-        # compare dealer hand with the remaining player hands
+        deal_player_hands
+        deal_dealer_hand
       end
+      # def start
+      #   validate_shoe_of_cards
+      #   validate_betting_boxes
+      #   # deal card for each hand in each betting boxes
+      #   # evaluate the result on each hand of betting box
+      #   # pay out the winning hands
+      #   # deal card for dealer
+      #   # compare dealer hand with the remaining player hands
+      # end
 
       def validate_shoe_of_cards
         raise 'Shoe can not be blank' unless shoe_of_cards
@@ -34,10 +42,16 @@ module BlackjackRuby
         end
       end
 
-      def deal_player_cards
+      def deal_player_hands
         betting_boxes.each do |betting_box|
           betting_box.deal_initial_hand([shoe.draw, shoe.draw])
         end
+      end
+
+      def deal_dealer_hand
+        raise 'Dealer Hand already dealt' unless dealer_hand.nil?
+
+        @dealer_hand = BlackjackRuby::Hand::DealerHand.new([shoe.draw, shoe.draw])
       end
       # def_delegators :@list, :count, :shuffle!, :shift, :pop, :empty?
     end
