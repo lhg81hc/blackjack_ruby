dd02ab57efbba759a72b8b21c62dbf9384a43311
Thad Le <llengocthangg@gmail.com>
2023-12-21 23:08:07 +1100

    Adjust PairSplittingStrategy
    - Change N to nil
    - Change Y to Split
    - Replace #split? by #find_move and #find_move!

--- lib/blackjack_ruby/strategies/pair_splitting_strategy.rb
+++ lib/blackjack_ruby/strategies/pair_splitting_strategy.rb
@@ -2,27 +2,31 @@ module BlackjackRuby
   module Strategies
     class PairSplittingStrategy < Base
       DEFAULT_STRATEGIES = {
-        'a,a' => { '2' => 'Y', '3' => 'Y', '4' => 'Y', '5' => 'Y', '6' => 'Y', '7' => 'Y', '8' => 'Y', '9' => 'Y', '10' => 'Y', 'a' => 'Y' },
-        '10,10' => { '2' => 'N', '3' => 'N', '4' => 'N', '5' => 'N', '6' => 'N', '7' => 'N', '8' => 'N', '9' => 'N', '10' => 'N', 'a' => 'N' },
-        '9,9' => { '2' => 'Y', '3' => 'Y', '4' => 'Y', '5' => 'Y', '6' => 'Y', '7' => 'N', '8' => 'Y', '9' => 'Y', '10' => 'N', 'a' => 'N' },
-        '8,8' => { '2' => 'Y', '3' => 'Y', '4' => 'Y', '5' => 'Y', '6' => 'Y', '7' => 'Y', '8' => 'Y', '9' => 'Y', '10' => 'Y', 'a' => 'Y' },
-        '7,7' => { '2' => 'Y', '3' => 'Y', '4' => 'Y', '5' => 'Y', '6' => 'Y', '7' => 'Y', '8' => 'N', '9' => 'N', '10' => 'N', 'a' => 'N' },
-        '6,6' => { '2' => 'Y', '3' => 'Y', '4' => 'Y', '5' => 'Y', '6' => 'Y', '7' => 'N', '8' => 'N', '9' => 'N', '10' => 'N', 'a' => 'N' },
-        '5,5' => { '2' => 'N', '3' => 'N', '4' => 'N', '5' => 'N', '6' => 'N', '7' => 'N', '8' => 'N', '9' => 'N', '10' => 'N', 'a' => 'N' },
-        '4,4' => { '2' => 'N', '3' => 'N', '4' => 'N', '5' => 'Y', '6' => 'Y', '7' => 'N', '8' => 'N', '9' => 'N', '10' => 'N', 'a' => 'N' },
-        '3,3' => { '2' => 'Y', '3' => 'Y', '4' => 'Y', '5' => 'Y', '6' => 'Y', '7' => 'Y', '8' => 'N', '9' => 'N', '10' => 'N', 'a' => 'N' },
-        '2,2' => { '2' => 'Y', '3' => 'Y', '4' => 'Y', '5' => 'Y', '6' => 'Y', '7' => 'Y', '8' => 'N', '9' => 'N', '10' => 'N', 'a' => 'N' }
+        'a,a' => { '2' => 'Split', '3' => 'Split', '4' => 'Split', '5' => 'Split', '6' => 'Split', '7' => 'Split', '8' => 'Split', '9' => 'Split', '10' => 'Split', 'a' => 'Split' },
+        '10,10' => { '2' => nil, '3' => nil, '4' => nil, '5' => nil, '6' => nil, '7' => nil, '8' => nil, '9' => nil, '10' => nil, 'a' => nil },
+        '9,9' => { '2' => 'Split', '3' => 'Split', '4' => 'Split', '5' => 'Split', '6' => 'Split', '7' => nil, '8' => 'Split', '9' => 'Split', '10' => nil, 'a' => nil },
+        '8,8' => { '2' => 'Split', '3' => 'Split', '4' => 'Split', '5' => 'Split', '6' => 'Split', '7' => 'Split', '8' => 'Split', '9' => 'Split', '10' => 'Split', 'a' => 'Split' },
+        '7,7' => { '2' => 'Split', '3' => 'Split', '4' => 'Split', '5' => 'Split', '6' => 'Split', '7' => 'Split', '8' => nil, '9' => nil, '10' => nil, 'a' => nil },
+        '6,6' => { '2' => 'Split', '3' => 'Split', '4' => 'Split', '5' => 'Split', '6' => 'Split', '7' => nil, '8' => nil, '9' => nil, '10' => nil, 'a' => nil },
+        '5,5' => { '2' => nil, '3' => nil, '4' => nil, '5' => nil, '6' => nil, '7' => nil, '8' => nil, '9' => nil, '10' => nil, 'a' => nil },
+        '4,4' => { '2' => nil, '3' => nil, '4' => nil, '5' => 'Split', '6' => 'Split', '7' => nil, '8' => nil, '9' => nil, '10' => nil, 'a' => nil },
+        '3,3' => { '2' => 'Split', '3' => 'Split', '4' => 'Split', '5' => 'Split', '6' => 'Split', '7' => 'Split', '8' => nil, '9' => nil, '10' => nil, 'a' => nil },
+        '2,2' => { '2' => 'Split', '3' => 'Split', '4' => 'Split', '5' => 'Split', '6' => 'Split', '7' => 'Split', '8' => nil, '9' => nil, '10' => nil, 'a' => nil }
       }.freeze
 
       def build_strategies
         DEFAULT_STRATEGIES
       end
 
-      def split?
-        found = strategies[player_card_ranks][dealer_up_card_rank]
+      def find_move
+        strategies.dig(player_card_ranks, dealer_up_card_rank)
+      end
+
+      def find_move!
+        found = find_move
         raise 'Unknown move' unless found
 
-        found == 'Y'
+        found
       end
     end
   end
