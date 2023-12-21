f4909e0606e803f135bf36fb764162522490976a
Thad Le <llengocthangg@gmail.com>
2023-12-21 23:11:27 +1100

    Adjust SoftTotalsStrategy
    - Change move abbreviation to full words
    - Add find_move! which will raise error if move is not found

--- lib/blackjack_ruby/strategies/soft_totals_strategy.rb
+++ lib/blackjack_ruby/strategies/soft_totals_strategy.rb
@@ -2,22 +2,26 @@ module BlackjackRuby
   module Strategies
     class SoftTotalsStrategy < Base
       DEFAULT_STRATEGIES = {
-        'a,9' => { '2' => 'S', '3' => 'S', '4' => 'S', '5' => 'S', '6' => 'S', '7' => 'S', '8' => 'S', '9' => 'S', '10' => 'S', 'a' => 'S' },
-        'a,8' => { '2' => 'S', '3' => 'S', '4' => 'S', '5' => 'S', '6' => 'D', '7' => 'S', '8' => 'S', '9' => 'S', '10' => 'S', 'a' => 'S' },
-        'a,7' => { '2' => 'D', '3' => 'D', '4' => 'D', '5' => 'D', '6' => 'D', '7' => 'S', '8' => 'S', '9' => 'H', '10' => 'H', 'a' => 'H' },
-        'a,6' => { '2' => 'H', '3' => 'D', '4' => 'D', '5' => 'D', '6' => 'D', '7' => 'H', '8' => 'H', '9' => 'H', '10' => 'H', 'a' => 'H' },
-        'a,5' => { '2' => 'H', '3' => 'H', '4' => 'D', '5' => 'D', '6' => 'D', '7' => 'H', '8' => 'H', '9' => 'H', '10' => 'H', 'a' => 'H' },
-        'a,4' => { '2' => 'H', '3' => 'H', '4' => 'D', '5' => 'D', '6' => 'D', '7' => 'H', '8' => 'H', '9' => 'H', '10' => 'H', 'a' => 'H' },
-        'a,3' => { '2' => 'H', '3' => 'H', '4' => 'H', '5' => 'D', '6' => 'D', '7' => 'H', '8' => 'H', '9' => 'H', '10' => 'H', 'a' => 'H' },
-        'a,2' => { '2' => 'H', '3' => 'H', '4' => 'H', '5' => 'D', '6' => 'D', '7' => 'H', '8' => 'H', '9' => 'H', '10' => 'H', 'a' => 'H' },
+        'a,9' => { '2' => 'Stay', '3' => 'Stay', '4' => 'Stay', '5' => 'Stay', '6' => 'Stay', '7' => 'Stay', '8' => 'Stay', '9' => 'Stay', '10' => 'Stay', 'a' => 'Stay' },
+        'a,8' => { '2' => 'Stay', '3' => 'Stay', '4' => 'Stay', '5' => 'Stay', '6' => 'Double', '7' => 'Stay', '8' => 'Stay', '9' => 'Stay', '10' => 'Stay', 'a' => 'Stay' },
+        'a,7' => { '2' => 'Double', '3' => 'Double', '4' => 'Double', '5' => 'Double', '6' => 'Double', '7' => 'Stay', '8' => 'Stay', '9' => 'Hit', '10' => 'Hit', 'a' => 'Hit' },
+        'a,6' => { '2' => 'Hit', '3' => 'Double', '4' => 'Double', '5' => 'Double', '6' => 'Double', '7' => 'Hit', '8' => 'Hit', '9' => 'Hit', '10' => 'Hit', 'a' => 'Hit' },
+        'a,5' => { '2' => 'Hit', '3' => 'Hit', '4' => 'Double', '5' => 'Double', '6' => 'Double', '7' => 'Hit', '8' => 'Hit', '9' => 'Hit', '10' => 'Hit', 'a' => 'Hit' },
+        'a,4' => { '2' => 'Hit', '3' => 'Hit', '4' => 'Double', '5' => 'Double', '6' => 'Double', '7' => 'Hit', '8' => 'Hit', '9' => 'Hit', '10' => 'Hit', 'a' => 'Hit' },
+        'a,3' => { '2' => 'Hit', '3' => 'Hit', '4' => 'Hit', '5' => 'Double', '6' => 'Double', '7' => 'Hit', '8' => 'Hit', '9' => 'Hit', '10' => 'Hit', 'a' => 'Hit' },
+        'a,2' => { '2' => 'Hit', '3' => 'Hit', '4' => 'Hit', '5' => 'Double', '6' => 'Double', '7' => 'Hit', '8' => 'Hit', '9' => 'Hit', '10' => 'Hit', 'a' => 'Hit' },
       }.freeze
 
       def build_strategies
         DEFAULT_STRATEGIES
       end
 
-      def move
-        found = strategies[player_card_ranks][dealer_up_card_rank]
+      def find_move
+        strategies.dig(player_card_ranks, dealer_up_card_rank)
+      end
+
+      def find_move!
+        found = find_move
         raise 'Unknown move' unless found
 
         found
