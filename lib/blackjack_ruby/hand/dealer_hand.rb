0ef4b3be59cc7510ed00d0bac34dd16988e6aa49
Thad Le <llengocthangg@gmail.com>
2023-12-27 02:26:22 +1100

    Remove redundant methods with Models::DealerOption

--- lib/blackjack_ruby/hand/dealer_hand.rb
+++ lib/blackjack_ruby/hand/dealer_hand.rb
@@ -12,18 +12,8 @@ module BlackjackRuby
         @up_card = cards.first
       end
 
-      def options
-        {
-          'stay' => can_stay?
-        }
-      end
-
       def enough?
         (soft_seventeen? && can_stay_on_soft_seventeen?) || hard_seventeen? || scores.any? { |s| s > 17 && s <= 21 }
-      end
-
-      def can_stay?
-        blackjack? || bust? || enough?
       end
 
       def hard_seventeen?
